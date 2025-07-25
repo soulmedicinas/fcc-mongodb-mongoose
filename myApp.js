88973dbf71fbdf3f72d633b5a5734bb9377d06e5
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-07-25 15:13:24 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
  require('dotenv').config();
-  const mongoose = require('mongoose');
-  mongoose.connect(process.env.MONGO_URI, {
+ const mongoose = require('mongoose');
+ mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
